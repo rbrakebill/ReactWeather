5fde89ae26e22e8d59bd92c46407c5d65affd8cc
rbrakebill <36460582+rbrakebill@users.noreply.github.com>
2020-10-11 17:10:01 -0500

    Initial commit

---
+++ app/api/openWeatherMap.jsx
@@ -0,0 +1,26 @@
+var axios = require("axios");
+
+const OPEN_WEATHER_MAP_URL =
+  "http://api.openweathermap.org/data/2.5/weather?appid=716757a3fb355e7a60f6b55b0100e08c&units=imperial";
+
+// 716757a3fb355e7a60f6b55b0100e08c
+
+module.exports = {
+  getTemp: function (location) {
+    var encodedLocation = encodeURIComponent(location);
+    var requestUrl = `${OPEN_WEATHER_MAP_URL}&q=${encodedLocation}`;
+
+    return axios.get(requestUrl).then(
+      function (res) {
+        if (res.data.cod && res.data.message) {
+          throw new Error(res.data.message);
+        } else {
+          return res.data.main.temp;
+        }
+      },
+      function (res) {
+        throw new Error(res.data.message);
+      }
+    );
+  },
+};
