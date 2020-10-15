ff8226ac805aa0c67ca58c0e11b1d6657334f277
rbrakebill <36460582+rbrakebill@users.noreply.github.com>
2020-10-14 20:23:28 -0500

    Setting up grid system, styling the Examples page

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,10 +1,19 @@
 var React = require('react');
+var {Link} = require('react-router')
 
 var Examples = (props) => {
     return (
         <div>
-            <h3>Examples</h3>
-            <p>Welcome to examples page!</p>
+            <h1 className="text-center">Examples</h1>
+            <p>Here are a few example locations to try out</p>
+            <ol>
+                <li>
+                    <Link to='/?location=Wichita'>Wichita, KS</Link>
+                </li>
+                <li>
+                    <Link to='?/location=Las Vegas'>Las Vegas, NV</Link>
+                </li>
+            </ol>
         </div>
     )
 };
