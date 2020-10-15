00f16875cf863405b73c562408564e0a5d3110d4
rbrakebill <36460582+rbrakebill@users.noreply.github.com>
2020-10-14 20:41:37 -0500

    Styling the about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,10 +1,19 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var About = (props) => {
     return (
         <div>
-            <h3>About</h3>
-            <p>Welcome to the about page</p>
+            <h1 className="text-center">About</h1>
+            <p className="text-center">This page was made with these technologies.<br />Check them out!</p>
+            <ul>
+                <li>
+                    <a href="https://reactjs.org/" target="_blank">ReactJS</a>
+                </li>
+                <li>
+                    <a href="https://openweathermap.org/" target="_blank">Open Weather Map</a>
+                </li>
+            </ul>
         </div>
     )
 };
