6326f2f15d51110c76d52d19da16d3128c712984
rbrakebill <36460582+rbrakebill@users.noreply.github.com>
2020-10-19 19:12:15 -0500

    Adding custom css

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -4,7 +4,7 @@ var {Link} = require('react-router');
 var About = (props) => {
     return (
         <div>
-            <h1 className="text-center">About</h1>
+            <h1 className="text-center page-title">About</h1>
             <p>Here are the tools used:</p>
             <ul>
                 <li>
